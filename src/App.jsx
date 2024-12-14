d258e90518017284386ea3059aeccb1b83864995
breinerLopez10 <breynerlopex234@gmail.com>
2024-12-14 18:02:31 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import './App.css'
 import {Routes, Route} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
+import Home from "./pages/Home";
+
 function App() {
 
   return (
     <div className="app-body">
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/employees" element={<Home />} />
-        <Route path="/employees/new" element={<Home />} /> */}
+        <Route path="/*" element={<Home />}/>
+        <Route path="/Home" element={<Home />} />
+
+
       </Routes>
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Navbar from '../components/layout/navbar/Navbar.jsx'
+import Footer from '../components/layout/footer/Footer.jsx'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+
+    El home
+
+
+    <Footer />
+
+    </>
+
+
+
+ 
+  )
+}
+
+export default Home
